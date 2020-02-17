78ba0497c9ecf7bcd063576d25a4e4e5bd7eb52a
Minto <rminto@aus2m-rminto.ad.ea.com>
2020-02-17 02:01:23 -0600

    fixed f'n

--- R/fake_helper_fun_example.R
+++ R/fake_helper_fun_example.R
@@ -3,12 +3,12 @@
 # fake example f'n takes an input df then uses sapply/lapply to map 
 # a function to every column of the input
 fake_helper_fun <- function(x) {
-  return(sapply(x, sum(is.na(x))))  
+  return(sapply(x, function(y) sum(is.na(y))))  
 }
 # f'n above gives total NULLs per column / variable, takes the sum of NULL values in a column, then
 # maps this function across every column
 
 helper_fun_logicals <- function(x) {
-  return(lapply(x, any(is.na(x))))
+  return(lapply(x, function(y) any(is.na(y))))
 }
 # similarly, but whether there is at least a single NULL per each column
