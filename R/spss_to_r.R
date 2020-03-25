1e91aaa575e69241724ed60edb3cb694189b2afb
Reid Minto <reid.m.minto@gmail.com>
2020-03-25 04:43:45 -0500

    updates

--- R/spss_to_r.R
+++ R/spss_to_r.R
@@ -1,7 +1,15 @@
 
+# To convert into spss to R functions
 
 select if (HH46  = 1).
 # df %>% filter(var == value)
 
 compute hhweightHH48 = HH48*hhweight.
 # df %>% mutate(hhweightHH48 = HH48 * hhweight)
+
+variable labels  total100 "Total".
+# variable labels are attribute labels
+
+
+value labels total100 100 " ".
+# value labels ***need to verify*** appear to be conditional recoding of vectors
